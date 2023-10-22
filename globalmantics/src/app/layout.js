f8f690a2c3d5b9db5ceeaa129836c8a128a0407b
kipkoech88 <hoseakipkoech88@gmail.com>
2023-10-22 23:04:17 +0300

    added fonts

--- globalmantics/src/app/layout.js
+++ globalmantics/src/app/layout.js
@@ -1,7 +1,12 @@
-import { Inter } from 'next/font/google'
+import { Inter, Open_Sans } from 'next/font/google'
 import './globals.css'
 
+
 const inter = Inter({ subsets: ['latin'] })
+const openSans = Open_Sans({
+  subsets: ['latin'],
+  weight: ['400'],
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -10,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={openSans.className}>
       <body className={inter.className}>{children}</body>
     </html>
   )
