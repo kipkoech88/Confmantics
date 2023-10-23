df90c955d286ae5e2077a42d6aaccbebb664c053
kipkoech88 <hoseakipkoech88@gmail.com>
2023-10-23 09:20:07 +0300

    updated layout'

--- globalmantics/src/app/layout.js
+++ globalmantics/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter, Open_Sans } from 'next/font/google'
 import './globals.css'
-
+import Link from 'next/link'
+import styles from './rootStyle.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 const openSans = Open_Sans({
@@ -16,7 +17,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={openSans.className}>
-      <body className={inter.className}>{children}</body>
+      <head/>
+      <body className={inter.className}>
+      <header>
+      <h2>
+        <Link className={styles.homeLink} href='/home'>Globalmantics</Link>
+      </h2>
+      <h2>
+        <Link className={styles.menuBar} href='/blog'>Blog</Link>
+      </h2>
+      <h2>
+        <Link className={styles.menuBar} href='/settings'>Settings</Link>
+      </h2>
+      <h2 className='text-large'>
+        <Link className={styles.menuBar} href='/conference'>Conference</Link>
+      </h2>
+      </header>
+      {children}
+      </body>
     </html>
   )
 }
