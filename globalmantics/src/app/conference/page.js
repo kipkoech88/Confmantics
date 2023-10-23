e766bcc390e9ce31cf78c4a2966264349bbfd8ce
kipkoech88 <hoseakipkoech88@gmail.com>
2023-10-23 15:03:24 +0300

    updated UI

--- globalmantics/src/app/conference/page.js
+++ globalmantics/src/app/conference/page.js
@@ -9,12 +9,12 @@ const page = () => {
       <div className={styles.bgWrap}>
         <Image src={ConfImage} fill alt='Our story' style={{objectFit: 'cover'}} placeholder="blur" sizes="100vw" quality={100}/>
       </div>
-      <h1>Welcome to Conference page</h1>
+      <h1 className={styles.bgHeader}>Welcome to Globalmantics Conference</h1>
 
-      <h2>
+      <h2 className={styles.bgText}>
         <Link href='/conference/speakers'>Speakers</Link>
       </h2>
-      <h2>
+      <h2 className={styles.bgText}>
         <Link href='/conference/sessions'>Sessions</Link>
       </h2>
     </>
--- globalmantics/src/app/home/page.js
+++ globalmantics/src/app/home/page.js
@@ -9,7 +9,7 @@ const page = () => {
       <div className={styles.bgWrap}>
         <Image src={bgImage} alt="Background  image" quality={100} placeholder='blur' sizes='100vw' fill style={{objectFit: 'cover'}} />
       </div>
-      <h1>Humble beginings, the story of life</h1>
+      <h1 className={styles.bgHeader}>Humble beginings, the story of life</h1>
       <p className={styles.bgText}>
         How we became farmers of the future, telling stories of tommorow today
       </p>
