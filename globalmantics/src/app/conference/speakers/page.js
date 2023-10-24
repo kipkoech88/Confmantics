4e6b66dfdff903a3ca06f087f2cb5f4b63d0eb92
kipkoech88 <hoseakipkoech88@gmail.com>
2023-10-24 14:34:26 +0300

    updated conference page

--- globalmantics/src/app/conference/speakers/page.js
+++ globalmantics/src/app/conference/speakers/page.js
@@ -1,9 +1,31 @@
 import Link from 'next/link'
+import styles from '../conference.module.css'
 
-const page = () => {
+async function fetchspeakers() {
+  const res = await fetch("https://raw.githubusercontent.com/adhithiravi/Consuming-Graphql-Apollo/master/api/data/speakers.json")
+
+  const data = await res.json()
+
+  return data
+}
+
+
+const page = async () => {
+
+  const data = await fetchspeakers()
+
   return (
-    <div>
+    <div className={styles.parentContainer}>
+      <div className='self-start whitespace-nowrap rounded-lg bg-gray-700 py-1 text-sm font-medium tabular-nums'>
+        Last Rendererd: {new Date().toLocaleTimeString()}
+      </div>
       <h1>Welcome to GlobalMantics Speakers</h1>
+      {data.speakers.map(({id, name, bio}) => (
+        <div key= {id} className={styles.infoContainer}>
+          <h3 className={styles.titleText}>{name}</h3>
+          <p className={styles.descText}>{bio}</p>
+        </div>
+      ))}
       <h2>
         <Link href='/conference'>Back to Conference</Link>
       </h2>
